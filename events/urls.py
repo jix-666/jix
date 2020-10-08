959b39945dc24552a230ee743bb54cf41c85f601
Anusid W <anusid.w@ku.th>
2020-10-08 15:40:22 +0700

    add new views and urls

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     path('', views.events, name='feed'),
+    path('<str:category>/', views.events_by_category, name='events_by_category')
+
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 
 # Create your views here.
 
 
 def events(request):
+    all_events = Event.objects.all()
     return HttpResponse('Events page')
+
+
+def events_by_category(request, category):
+    events_by_category = Event.objects.filter(category=category)
+    return HttpResponse('Event ' + category)
