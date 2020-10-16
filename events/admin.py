7f5ec093185459af6f2f187f46f86574a9ad101d
Tatpol <tatpol.s@ku.th>
2020-10-16 16:35:46 +0700

    Fix bug (forget :)

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+
 from .models import Event
+
 
 # Register your models here.
 
-class EventAdmin(admin.ModelAdmin)
+class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+
 
 admin.site.register(Event, EventAdmin)
