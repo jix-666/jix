f5bf2cce5c53e41a177eed2e1f6afc5eb483f3bb
SirapopKunjiak <sirapop.ku@ku.th>
2020-10-16 17:21:36 +0700

    add CRUD(Retrieve) View for user to view detail of events

--- events/views.py
+++ events/views.py
@@ -13,14 +13,14 @@ def events(request):
     return render(request, 'events/all_events.html', {'all_events': all_events, 'all_events_active': True})
 
 
-def events_by_category(request, category):
-    events_in_category = Event.objects.filter(category=category)
+def events_by_category(request, event_category):
+    events_in_category = Event.objects.filter(category=event_category)
     return render(
         request,
         'events/events_by_category.html',
         {
             'events_in_category': events_in_category,
-            'category': category,
+            'category': event_category,
         }
     )
 
@@ -38,3 +38,8 @@ def new_event(request):
     else:
         event_form = EventForm()
     return render(request, 'events/new_event.html', {'event_form': event_form})
+
+
+def event_detail(request, event_category, event_slug):
+    event = Event.objects.get(slug=event_slug)
+    return render(request, 'events/event_detail.html', {'event': event})
