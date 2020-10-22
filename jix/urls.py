5aa068d3a6985ed130f92ac0f2a7ab268996d219
Anusid W <anusid.w@ku.th>
2020-10-22 13:55:49 +0700

    improve nav bar and index page

--- jix/urls.py
+++ jix/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name="index"),
     path('events/', include('events.urls')),
 ]
