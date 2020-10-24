70eba79bf1eae1530318cef497753ac093cd56ff
Tatpol <tatpol.s@ku.th>
2020-10-24 20:45:04 +0700

    Minor change on Report model

--- report/models.py
+++ report/models.py
@@ -6,16 +6,16 @@ from django.utils import timezone
 REPORT_TYPE = [
     ('', '----------'),
     ('nonworking', "Something isn't Working"),
-    ('sexual_abusive', 'Sexually explicit event'),
+    ('sexual-abusive', 'Sexually explicit event'),
     ('spam', 'Spam or scam'),
-    ('hate_speech', 'Hate Speech'),
+    ('hate-speech', 'Hate Speech'),
     ('violence', 'Violence or Harmful event'),
 ]
 
 
 class Report(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)  # event class
-    report_type = models.TextField(max_length=15, choices=REPORT_TYPE, default='')
+    report_type = models.CharField(max_length=15, choices=REPORT_TYPE, default='')
     detail = models.TextField(max_length=500)  # detail of the report
     reported_at = models.DateTimeField(default=timezone.now)  # reported date of the event
     slug = models.SlugField(unique=True)
