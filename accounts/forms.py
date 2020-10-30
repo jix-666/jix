38fa45da9845feb5b783d24fe91896b230ce6086
Tatpol <tatpol.s@ku.th>
2020-10-31 01:24:30 +0700

    Improve form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,19 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class UserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email', 'password1', 'password2']
-        widgets = {'username': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}, ),
-                   'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
-                   'email': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
-                   'password1': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
-                   'password2': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}), }
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
