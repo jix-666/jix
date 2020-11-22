3343914de1dec26f859bb4c0d85f7c0a382b416c
Tatpol Samakpong <tatpol.s@ku.th>
2020-11-23 01:32:27 +0700

    Added link in list of attendees to user profile in event detail page

--- users/views.py
+++ users/views.py
@@ -14,31 +14,25 @@ def profile_page(request, username):
         messages.warning(request, 'User not found.')
         return redirect('events:feed')
     user_profile = UserProfile.objects.get(user=user)
-    if user_profile.user == request.user and request.user.is_authenticated:
-        profile_form = ProfileForm(initial={
-            'user_name': user_profile.user.username,
-            'first_name': user_profile.user.first_name,
-            'email': user_profile.user.email,
-            'profile_pic': user_profile.user.userprofile.profile_picture
-        })
-        if request.method == 'POST':
-            profile_form = ProfileForm(request.POST, request.FILES)
-
-            if profile_form.is_valid():
-                if user_profile.user == request.user and request.user.is_authenticated:
-                    user_username = profile_form.cleaned_data['user_name']
-                    user_profile.user.username = user_username
-                    user_profile.user.email = profile_form.cleaned_data['email']
-                    user_profile.user.first_name = profile_form.cleaned_data['first_name']
-                    user_profile.profile_picture = profile_form.cleaned_data['profile_pic']
-                    user_profile.save()
-                    messages.info(request, f'{username} profile was updated.')
-                    return redirect('users:profile', username)
-                else:
-                    messages.warning(request, f'You can not edit {username} profile.')
-    else:
-        messages.warning(request, f'You can not edit {username} profile.')
-        return redirect('events:feed')
-
+    profile_form = ProfileForm(initial={
+        'user_name': user_profile.user.username,
+        'first_name': user_profile.user.first_name,
+        'email': user_profile.user.email,
+        'profile_pic': user_profile.user.userprofile.profile_picture
+    })
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if profile_form.is_valid():
+            if user_profile.user == request.user and request.user.is_authenticated:
+                user_username = profile_form.cleaned_data['user_name']
+                user_profile.user.username = user_username
+                user_profile.user.email = profile_form.cleaned_data['email']
+                user_profile.user.first_name = profile_form.cleaned_data['first_name']
+                user_profile.profile_picture = profile_form.cleaned_data['profile_pic']
+                user_profile.save()
+                messages.info(request, f'{username} profile was updated.')
+                return redirect('users:profile', username)
+            else:
+                messages.warning(request, f'You can not edit {username} profile.')
     return render(request, 'users/profile.html',
                   {'user_profile': user_profile, 'profile_form': profile_form})
