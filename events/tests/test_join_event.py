1eb0106686a9f15c5f0afc21abf844bdf677f529
Anusid W <anusid.w@ku.th>
2020-11-30 04:07:58 +0700

    edit test to suit new feature

--- events/tests/test_join_event.py
+++ events/tests/test_join_event.py
@@ -9,43 +9,54 @@ class JoinEventTests(TestCase):
     """Tests for Joining event."""
 
     def test_single_user_joins_event(self):
-        """If the user joins the event. 'You' will be shown on attendees. (if there are viewing for their account)."""
+        """If the user joins the event. Username will be shown on attendees. (if there are viewing for their account)."""
         user = User.objects.create_user(username='testuser', password='secret123456')
+        user28 = User.objects.create_user(username='testuser28', password='secret123456')
         self.client.post(reverse('login'), {'username': 'testuser',
                                             'password': 'secret123456'}, follow=True)
         event1 = create_event("Walking", "Walk in Jungle", "Sport", user)
+        self.client.get(reverse('logout'))
+        self.client.post(reverse('login'), {'username': 'testuser28',
+                                            'password': 'secret123456'}, follow=True)
         url = reverse('events:joining_event', args=(event1.category, event1.slug))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/events/')
         url = reverse('events:event_detail', args=(event1.category, event1.slug))
         response = self.client.get(url)
-        self.assertContains(response, 'You')
+        self.assertContains(response, f'{user28.username}')
 
     def test_2_or_more_users_join_event(self):
         """If there are two or more users join the event. Their name will be shown on attendees."""
         # create users and event
         user1 = User.objects.create_user(username='testuser', password='secret123456')
         user2 = User.objects.create_user(username='testuser2', password='secret1234567')
+        user28 = User.objects.create_user(username='testuser28', password='secret1234567')
         # user 1 login
         self.client.post(reverse('login'), {'username': 'testuser',
                                             'password': 'secret123456'}, follow=True)
         event1 = create_event("Swim", "Swim in the sea", "Sport", user2)
-        url = reverse('events:joining_event', args=(event1.category, event1.slug))
-        self.client.get(url)
         # user 1 logout
         self.client.get(reverse('logout'))
-
         # user 2 login
         self.client.post(reverse('login'), {'username': 'testuser2',
                                             'password': 'secret1234567'}, follow=True)
+        url = reverse('events:joining_event', args=(event1.category, event1.slug))
+        self.client.get(url)
+        # user 2 logout
+        self.client.get(reverse('logout'))
+
+        # user 28 login
+        self.client.post(reverse('login'), {'username': 'testuser28',
+                                            'password': 'secret1234567'}, follow=True)
+        url = reverse('events:joining_event', args=(event1.category, event1.slug))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/events/')
         url = reverse('events:event_detail', args=(event1.category, event1.slug))
         response = self.client.get(url)
         self.assertContains(response, f'{user1.username}')
-        self.assertContains(response, 'You')
+        self.assertContains(response, f'{user28.username}')
 
     def test_unauthenticated_user_join_event(self):
         """If the users is not authenticated and try to invoke joining, they will be redirect to login page."""
